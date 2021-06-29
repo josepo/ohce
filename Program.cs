f8fd135826df74c489c42d6accc13aba95fe0405
josepo <jlpolo@gmail.com>
2021-06-29 19:15:34 +0200

    Hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ohce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
